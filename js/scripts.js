1e351414a4f0584ce4b3dceeea62bc502f108e2e
Randel M <Randel.C.Moore@gmail.com>
2020-10-19 14:40:28 -0700

    add multiply and divide to js code
    co-authored-by: Svea Wade <svealinneawade@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function add(number1, number2) {
+  return number1 + number2;
+  }
+function subtract(number1, number2) {
+  return number1 - number2;
+}  
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+function divide(number1, number2) {
+  return number1 / number2;
+}
+const number1 = parseInt(prompt("Enter a number"));
+const number2 = parseInt(prompt("Enter another number"));
+const result1 = add(number1,number2);
+const result2 = subtract(number1,number2);
+const result3 = multiply(number1, number2);
+const result4 = divide(number1, number2);
+alert(result4);
+
+  
